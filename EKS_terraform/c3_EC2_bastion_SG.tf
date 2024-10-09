bb9e0cc75768d0f60694d16d3efb87745a348145
zaynab <zeinabmohammed817@gmail.com>
2024-10-10 02:41:24 +0300

    add port 8080 to SEC-Grp

--- EKS_terraform/c3_EC2_bastion_SG.tf
+++ EKS_terraform/c3_EC2_bastion_SG.tf
@@ -1,17 +1,40 @@
 # AWS EC2 Security Group Terraform Module
 # Security Group for Public Bastion Host
-module "public_bastion_sg" {
-  source  = "terraform-aws-modules/security-group/aws"  
-  version = "4.17.2"
+
+resource "aws_security_group" "ec2_bastion_SG" {
 
   name = "public-bastion-sg"
-  description = "Security Group with SSH port open for everybody (IPv4 CIDR), egress ports are all world open"
   vpc_id = module.vpc.vpc_id
-  # Ingress Rules & CIDR Blocks
-  ingress_rules = ["ssh-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  # Egress Rule - all-all open
-  egress_rules = ["all-all"]
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+  ##_for_jenkins_## 
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+  egress  {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+
+
+
 
 
